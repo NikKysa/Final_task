efcce0402ad656b4f190403b3dda3ffaf3b2df98
Николай <sisse87@mail.ru>
2022-10-03 23:52:49 +0300

    prog_ready

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите массив строк через пробел");
+string[] array = Console.ReadLine()!.Split(' ');
+string[] result = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3 && !string.IsNullOrWhiteSpace(array[i])) 
+    Console.Write(result[i] = array[i] + " ");   
+}
